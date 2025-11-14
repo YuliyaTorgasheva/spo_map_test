9ec192fc40ab7e8763232b185f061674465f52e2
YuliyaTorgasheva <yuliya.torgasheva@gmail.com>
2025-11-14 12:25:17 +0400

    update

--- main.js
+++ main.js
@@ -235,7 +235,7 @@ Promise.all([
                 <div class="card-info">
                 <p><strong>Регион:</strong> ${props.region_name || '-'}</p>
                 <p><strong>territory_ud:</strong> ${props.territory_id}</p>
-                <p><strong>Индекс доступности СПО:</strong> ${ugsCount}</p>
+                <p><strong>Индекс доступности СПО (число доступных УГС):</strong> ${ugsCount}</p>
                 <p><strong>Население 15–25:</strong> ${props.population_15_25 || '-'}</p>
                 <p><strong>СПО, включая соседей до 50 км:</strong> ${props.unique_colleges_0_50 || 0}</p>
                 <p><strong>Доступно УГС, включая соседей до 50 км:</strong> ${ugsCount}</p>
@@ -386,7 +386,7 @@ Promise.all([
                 <div style="width:20px;height:12px;background:#006d2c;margin-right:5px;"></div> <span>>37</span>
             </div>
         `;
-        document.body.appendChild(legend);
+        document.getElementById('map').appendChild(legend);
 
     });
 });
